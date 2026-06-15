2ad0cef691c2feff26c6421550cf082e6370bf13
LB <lboyko@student.mitt.ca>
2026-06-15 10:20:26 -0500

    Changed products state location to main app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import SearchProducts from "./components/SearchProducts";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const url = "https://fakestoreapi.com/products/";
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(url);
+        console.log('Data from axios:');
+        setProducts(response.data);
+      }
+      catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <SearchProducts />
+      <SearchProducts products={products} />
     </>
   );
 }
--- src/components/SearchProducts.js
+++ src/components/SearchProducts.js
@@ -1,31 +1,9 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-function SearchProducts()
+function SearchProducts(props)
 {
-  const [products, setProducts] = useState([]);
-  const url = "https://fakestoreapi.com/products/";
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await axios.get(url);
-        console.log('Data from axios:');
-        setProducts(response.data);
-      }
-      catch (error) {
-        console.log(error.message);
-      }
-    }
-    fetchProducts();
-  }, []);
-  
-
-
   return(
     <>
       <ul>
-        {products.map(link => (
+        {props.products.map(link => (
           <li key={link.id}>
             <p>{link.title}</p>
           </li>
