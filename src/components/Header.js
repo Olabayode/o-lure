c800343e38cdd1f5bae19c7c3e1b7635416e88c8
Tracy <trac.boats.tb@gmail.com>
2026-06-18 17:56:58 -0500

    changes to header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,19 @@ function Header()
 {
   return(
     <header className="site-header">
-      <h1>o-lure</h1>
-      <nav>
-        <NavLink to="/o-lure/">Home</NavLink>
-        <NavLink to="/o-lure/products">Products</NavLink>
-        <NavLink to="/o-lure/search">Search</NavLink>
-        <NavLink to="/o-lure/cart">Cart</NavLink>
-      </nav>
-    </header>
+
+    <div className="logo">
+      O-LURE
+    </div>
+
+    <nav className="main-nav">
+      <NavLink to="/o-lure/">Home</NavLink>
+      <NavLink to="/o-lure/products">Shop</NavLink>
+      <NavLink to="/o-lure/search">Search</NavLink>
+      <NavLink to="/o-lure/cart">Cart</NavLink>
+    </nav>
+
+</header>
   );
 }
 
