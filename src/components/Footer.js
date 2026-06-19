76d93701f2223060189b88e9dd9bbd9c25d58fc5
Tracy <trac.boats.tb@gmail.com>
2026-06-19 00:38:21 -0500

    footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,49 @@
-import { Link } from 'react-router-dom';
 
 function Footer()
 {
   return(
+    <>
     <footer className="site-footer">
 
-      <div className="footer-brand">
-        <h2>O-LURE</h2>
-        <p>
-          Timeless fashion designed for everyday elegance.
-        </p>
-      </div>
+        <div className="footer-brand">
+            <h2>O-LURE</h2>
 
-      <div>
-        <h3>Shop</h3>
-        <Link to="/o-lure/products">New Arrivals</Link>
-        <Link to="/o-lure/products">Women</Link>
-        <Link to="/o-lure/products">Men</Link>
-      </div>
+            <p>
+            Timeless fashion designed for everyday elegance.
+            </p>
+        </div>
 
-      <div>
-        <h3>Support</h3>
-        <a href="/">Shipping</a>
-        <a href="/">Returns</a>
-        <a href="/">Contact</a>
-      </div>
+        <div>
+            <h3>Shop</h3>
 
-      <div>
-        <h3>Follow</h3>
-        <a href="/">Instagram</a>
-        <a href="/">Pinterest</a>
-        <a href="/">TikTok</a>
-      </div>
+            <a href="/">Women</a>
+            <a href="/">Men</a>
+            <a href="/">Jewelry</a>
+        </div>
+
+        <div>
+            <h3>Customer Care</h3>
+
+            <a href="/">Shipping</a>
+            <a href="/">Returns</a>
+            <a href="/">Contact</a>
+        </div>
+
+        <div>
+            <h3>Follow</h3>
+
+            <a href="/">Instagram</a>
+            <a href="/">Pinterest</a>
+            <a href="/">TikTok</a>
+        </div>
 
     </footer>
+
+<div className="footer-bottom">
+  <p>© 2026 O-LURE. All rights reserved.</p>
+</div>
+    </>
+
   );
 }
 
