7f058f278f6bcf81c9fec434a65d4fe5c259392a
Emmanuel Olure <emmanuelolabayode@gmail.com>
2026-06-19 07:34:55 -0500

    worked on the hero banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,6 +1,7 @@
 import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import heroBannerImage from '../assets/heroBanner.png';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -21,6 +22,22 @@ function HeroBanner(props)
 
   return(
     <section className="hero-banner">
+      <div
+        className="hero-brand-banner"
+        style={{'--hero-banner-image': `url(${heroBannerImage})`}}
+      >
+        <div className="hero-brand-content">
+          <h1>
+            <span>O</span>-LURE
+          </h1>
+          <p>Confidence Wears O-LURE</p>
+          <div className="hero-brand-actions">
+            <button type="button">About Us</button>
+            <Link to="/o-lure/products">Shop Now</Link>
+          </div>
+        </div>
+      </div>
+
       <Swiper
         modules={[Autoplay, Navigation, Pagination]}
         navigation
